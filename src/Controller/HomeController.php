eacf54a24a38da62e8f37e76aa349a4b3f07482f
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-07-15 16:33:33 +0200

    making frontend progress

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            // 'controller_name' => 'HomeController',
         ]);
     }
 }
--- src/Controller/UniversitiesController.php
+++ src/Controller/UniversitiesController.php
@@ -5,14 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\University;
+use App\Entity\Degree;
 
 class UniversitiesController extends AbstractController
 {
     #[Route('/universities', name: 'app_universities')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $universities = $entityManager->getRepository(University::class)->findAll();
+
         return $this->render('universities/index.html.twig', [
-            'controller_name' => 'UniversitiesController',
+            'universities' => $universities,
+        ]);
+    }
+
+    #[Route('/{universitySlug}', name: 'app_university')]
+    public function show(string $universitySlug, EntityManagerInterface $entityManager): Response
+    {
+        $university = $entityManager->getRepository(University::class)
+            ->findOneBy(['slug' => $universitySlug]);
+
+        $degrees = $entityManager->getRepository(Degree::class)
+            ->findBy(['university' => $university]);
+
+        if (!$university) {
+            throw $this->createNotFoundException('La universidad especificada no existe');
+        }
+
+        return $this->render('universities/show.html.twig', [
+            'university' => $university,
+            'degrees' => $degrees,
         ]);
     }
 }
