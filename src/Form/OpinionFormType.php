c1ba71a91ddd58b1b88b73477e135b442ec1639e
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-07-19 13:44:38 +0200

    mispell fix

--- src/Form/OpinionFormType.php
+++ src/Form/OpinionFormType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\Opinion;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,7 +15,7 @@ class OpinionFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('comment', TextAreaType::class, ['required' => false])
+            ->add('comment', TextareaType::class, ['required' => false])
             // ->add('keywords')
             // ->add('reviewed')
             // ->add('accepted')
