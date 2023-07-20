ac9287679657e9cf40cb1b94fdcb7afe4c96d428
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-07-20 23:53:36 +0200

    mailjet testing

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('send@test.com', 'UniOpinions Bot'))
+                    ->from(new Address('noreply@uniopinions.com', 'UniOpinions Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
