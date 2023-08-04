3aa172585a4917639ac2dbf4eaef8b9e9080f423
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-08-04 19:30:55 +0200

    added about project page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,12 @@ class HomeController extends AbstractController
             // 'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/about', name: 'app_about')]
+    public function showAbout(): Response
+    {
+        return $this->render('about.html.twig', [
+            // 'controller_name' => 'HomeController',
+        ]);
+    }
 }
