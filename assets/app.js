46a070b12e9d42c802d3fc92d8c3d5b35de3e5e9
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-08-04 19:30:30 +0200

    added comment rules to forms using a11y-dialog

--- assets/app.js
+++ assets/app.js
@@ -11,6 +11,27 @@ import 'select2/dist/css/select2.min.css';
 import './select2_init'; 
 import './styles/customSelect.css';
 import 'select2/dist/js/i18n/es';
+import A11yDialog from 'a11y-dialog'
+
+var dialogEl = document.getElementById('my-dialog')
+
+// Comprobar si dialogEl existe
+if (dialogEl) {
+    var dialog = new A11yDialog(dialogEl)
+
+    dialog.on('show', function (event) {
+        const container = event.target
+
+        // And if the event is the result of a UI interaction (i.e. was not triggered
+        // programmatically via `.show(..)`), the `detail` prop contains the original
+        // event
+        const target = event.detail.target
+        const opener = target.closest('[data-a11y-dialog-show]')
+
+        console.log(container, target, opener)
+    })
+}
+
 
 // Icons and img
 const sunIcon = document.getElementById("sun");
