9f35f1eeeaadf858ae1fc97462231b6c02eb80d9
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-08-25 13:12:07 +0200

    fixing easyadmin errors 2

--- src/Entity/Subject.php
+++ src/Entity/Subject.php
@@ -61,6 +61,13 @@ class Subject
         return $this->id;
     }
 
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+        
+        return $this;
+    }
+
     public function getName(): ?string
     {
         return $this->name;
